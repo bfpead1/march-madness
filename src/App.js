80261fbece104e51f2bef4a5e5f6263f887d75a6
bfpead1 <blake.pead@gmail.com>
2026-04-15 16:36:43 -0600

    redid to make sure all requirements are met

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
+import teamsData from './CollegeBasketballTeams.json';
+
+function Heading() {
+  return (
+    <div className="heading">
+      <h1>NCAA Basketball Teams</h1>
+      <p>Browse all college basketball teams competing in the NCAA. Find your favorite school, mascot, and location below.</p>
+    </div>
+  );
+}
+
+function TeamCard({ team }) {
+  return (
+    <div className="team-card">
+      <h2>{team.school}</h2>
+      <p className="mascot">{team.name}</p>
+      <p className="location">{team.city}, {team.state}</p>
+    </div>
+  );
+}
+
+function TeamList({ teams }) {
+  return (
+    <div className="team-list">
+      {teams.map((team) => (
+        <TeamCard key={team.tid} team={team} />
+      ))}
+    </div>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Heading />
+      <TeamList teams={teamsData.teams} />
     </div>
   );
 }
